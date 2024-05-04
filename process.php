5e8a16a33825739c3445e55c1a28cb6ff73fceeb
Akram sif allah Touati <52489552+akram400@users.noreply.github.com>
2024-05-04 12:56:22 +0200

    Update process.php

--- process.php
+++ process.php
@@ -10,21 +10,27 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// SQL query to fetch data from your table
-$sql = "SELECT * FROM contact";
-$result = $conn->query($sql);
+// Check if form data is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-// Check if there are results
-if ($result->num_rows > 0) {
-  // Output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "Name: " . $row["name"] . " - Email: " . $row["email"] . " - Subject: " . $row["Subject"] . " - Message: " . $row["Message"] . "<br>";
-  }
-} else {
-    echo "0 results";
+    // SQL query to insert data into the database
+    $sql = "INSERT INTO contact (name, email, Subject, Message) VALUES ('$name', '$email', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
+
+// Close the database connection
 $conn->close();
 ?>
