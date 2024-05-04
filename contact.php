1ad4e135c64ba4e848681c0fc477e90d26738d7e
Akram sif allah Touati <52489552+akram400@users.noreply.github.com>
2024-05-04 13:02:25 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,13 +3,13 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
   $name=$_POST['name'];
   $email=$_POST['email'];
   $subject=$_POST['subject'];
-  $msg=$_POST['message'];
+  $msg=$_POST['msg'];
 }
 
 $con=new mysqli('localhost','root','','agency_db');
 if($con){
   //echo"Connection Successful";
-   $sql="insert into `data`(name,email,subject,message) values('$name','$email','$subject','$msg')";
+   $sql="insert into `data`(name,email,subject,msg) values('$name','$email','$subject','$msg')";
    $result=mysqli_query($con,$sql);
    if($result){
     echo"Data inserted Successfuly";
